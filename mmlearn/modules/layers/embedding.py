fb7f8b39110e6b11ddc0e4ea8d050152e15362c2
fcogidi <41602287+fcogidi@users.noreply.github.com>
2024-09-18 10:11:29 -0400

    Rearrange vision layers and modules

--- mmlearn/modules/layers/embedding.py
+++ mmlearn/modules/layers/embedding.py
@@ -1,10 +1,200 @@
 """Embedding layers utilities."""
 
-from typing import Tuple
+from typing import List, Tuple
 
 import numpy as np
 import torch
 from torch import nn
+
+
+def patchify(batch: torch.Tensor, patch_size: Tuple[int, int]) -> torch.Tensor:
+    """Patchify a batch of images.
+
+    Parameters
+    ----------
+    batch : torch.Tensor
+        Batch of images.
+    patch_size : tuple of int
+        The size of the patch.
+
+    Returns
+    -------
+    torch.Tensor
+        Patchified batch.
+
+    Notes
+    -----
+    - Input shape: (b, h, w, c)
+    - Output shape: (b, nh, nw, ph, pw, c)
+
+    """
+    b, c, h, w = batch.shape
+    ph, pw = patch_size
+    nh, nw = h // ph, w // pw
+
+    batch_patches = torch.reshape(batch, (b, c, nh, ph, nw, pw))
+    return torch.permute(batch_patches, (0, 1, 2, 4, 3, 5))
+
+
+class PatchEmbed(nn.Module):
+    """
+    Image to Patch Embedding.
+
+    This module divides an image into patches and embeds them as a sequence of vectors.
+
+    Parameters
+    ----------
+    img_size : int, optional, default=224
+        Size of the input image (assumed to be square).
+    patch_size : int, optional, default=16
+        Size of each image patch (assumed to be square).
+    in_chans : int, optional, default=3
+        Number of input channels in the image.
+    embed_dim : int, optional, default=768
+        Dimension of the output embeddings.
+
+    """
+
+    def __init__(
+        self,
+        img_size: int = 224,
+        patch_size: int = 16,
+        in_chans: int = 3,
+        embed_dim: int = 768,
+    ) -> None:
+        super().__init__()
+        num_patches = (img_size // patch_size) * (img_size // patch_size)
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.num_patches = num_patches
+
+        self.proj = nn.Conv2d(
+            in_chans, embed_dim, kernel_size=patch_size, stride=patch_size
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass to convert an image into patch embeddings."""
+        return self.proj(x).flatten(2).transpose(1, 2)
+
+
+class ConvEmbed(nn.Module):
+    """
+    3x3 Convolution stems for ViT following ViTC models.
+
+    This module builds convolutional stems for Vision Transformers (ViT)
+    with intermediate batch normalization and ReLU activation.
+
+    Parameters
+    ----------
+    channels : List[int]
+        List of channel sizes for each convolution layer.
+    strides : List[int]
+        List of stride sizes for each convolution layer.
+    img_size : int, optional, default=224
+        Size of the input image (assumed to be square).
+    in_chans : int, optional, default=3
+        Number of input channels in the image.
+    batch_norm : bool, optional, default=True
+        Whether to include batch normalization after each convolution layer.
+
+    """
+
+    def __init__(
+        self,
+        channels: List[int],
+        strides: List[int],
+        img_size: int = 224,
+        in_chans: int = 3,
+        batch_norm: bool = True,
+    ) -> None:
+        super().__init__()
+        # Build the stems
+        stem = []
+        channels = [in_chans] + channels
+        for i in range(len(channels) - 2):
+            stem += [
+                nn.Conv2d(
+                    channels[i],
+                    channels[i + 1],
+                    kernel_size=3,
+                    stride=strides[i],
+                    padding=1,
+                    bias=(not batch_norm),
+                )
+            ]
+            if batch_norm:
+                stem += [nn.BatchNorm2d(channels[i + 1])]
+            stem += [nn.ReLU(inplace=True)]
+        stem += [
+            nn.Conv2d(channels[-2], channels[-1], kernel_size=1, stride=strides[-1])
+        ]
+        self.stem = nn.Sequential(*stem)
+
+        # Compute the number of patches
+        stride_prod = int(np.prod(strides))
+        self.num_patches = (img_size // stride_prod) ** 2
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass through the convolutional embedding layers."""
+        p = self.stem(x)
+        return p.flatten(2).transpose(1, 2)
+
+
+class Img2Seq(nn.Module):
+    """Convert a batch of images to a batch of sequences.
+
+    Parameters
+    ----------
+    img_size : tuple of int
+        The size of the input image.
+    patch_size : tuple of int
+        The size of the patch.
+    n_channels : int
+        The number of channels in the input image.
+    d_model : int
+        The dimension of the output sequence.
+
+    Notes
+    -----
+    - Input shape: (b, h, w, c)
+    - Output shape: (b, s, d)
+
+    """
+
+    def __init__(
+        self,
+        img_size: Tuple[int, int],
+        patch_size: Tuple[int, int],
+        n_channels: int,
+        d_model: int,
+    ) -> None:
+        """Initialize the Img2Seq module."""
+        super().__init__()
+        self.patch_size = patch_size
+        self.img_size = img_size
+
+        nh, nw = img_size[0] // patch_size[0], img_size[1] // patch_size[1]
+        n_tokens = nh * nw
+
+        token_dim = patch_size[0] * patch_size[1] * n_channels
+        self.linear = nn.Linear(token_dim, d_model)
+        self.cls_token = nn.Parameter(torch.randn(1, 1, d_model))
+        self.pos_emb = nn.Parameter(torch.randn(n_tokens, d_model))
+
+    def __call__(self, batch: torch.Tensor) -> torch.Tensor:
+        """Convert a batch of images to a batch of sequences."""
+        batch = patchify(batch, self.patch_size)
+
+        b, c, nh, nw, ph, pw = batch.shape
+
+        # Flattening the patches
+        batch = torch.permute(batch, [0, 2, 3, 4, 5, 1])
+        batch = torch.reshape(batch, [b, nh * nw, ph * pw * c])
+
+        batch = self.linear(batch)
+        cls: torch.Tensor = self.cls_token.expand([b, -1, -1])
+        emb: torch.Tensor = batch + self.pos_emb
+        return torch.cat([cls, emb], axis=1)
 
 
 def get_2d_sincos_pos_embed(
@@ -120,89 +310,3 @@ def get_1d_sincos_pos_embed_from_grid(embed_dim: int, pos: np.ndarray) -> np.nda
     emb_cos = np.cos(out)  # (M, D/2)
 
     return np.concatenate([emb_sin, emb_cos], axis=1)
-
-
-def patchify(batch: torch.Tensor, patch_size: Tuple[int, int]) -> torch.Tensor:
-    """Patchify a batch of images.
-
-    Parameters
-    ----------
-    batch : torch.Tensor
-        Batch of images.
-    patch_size : tuple of int
-        The size of the patch.
-
-    Returns
-    -------
-    torch.Tensor
-        Patchified batch.
-
-    Notes
-    -----
-    - Input shape: (b, h, w, c)
-    - Output shape: (b, nh, nw, ph, pw, c)
-
-    """
-    b, c, h, w = batch.shape
-    ph, pw = patch_size
-    nh, nw = h // ph, w // pw
-
-    batch_patches = torch.reshape(batch, (b, c, nh, ph, nw, pw))
-    return torch.permute(batch_patches, (0, 1, 2, 4, 3, 5))
-
-
-class Img2Seq(nn.Module):
-    """Convert a batch of images to a batch of sequences.
-
-    Parameters
-    ----------
-    img_size : tuple of int
-        The size of the input image.
-    patch_size : tuple of int
-        The size of the patch.
-    n_channels : int
-        The number of channels in the input image.
-    d_model : int
-        The dimension of the output sequence.
-
-    Notes
-    -----
-    - Input shape: (b, h, w, c)
-    - Output shape: (b, s, d)
-
-    """
-
-    def __init__(
-        self,
-        img_size: Tuple[int, int],
-        patch_size: Tuple[int, int],
-        n_channels: int,
-        d_model: int,
-    ) -> None:
-        """Initialize the Img2Seq module."""
-        super().__init__()
-        self.patch_size = patch_size
-        self.img_size = img_size
-
-        nh, nw = img_size[0] // patch_size[0], img_size[1] // patch_size[1]
-        n_tokens = nh * nw
-
-        token_dim = patch_size[0] * patch_size[1] * n_channels
-        self.linear = nn.Linear(token_dim, d_model)
-        self.cls_token = nn.Parameter(torch.randn(1, 1, d_model))
-        self.pos_emb = nn.Parameter(torch.randn(n_tokens, d_model))
-
-    def __call__(self, batch: torch.Tensor) -> torch.Tensor:
-        """Convert a batch of images to a batch of sequences."""
-        batch = patchify(batch, self.patch_size)
-
-        b, c, nh, nw, ph, pw = batch.shape
-
-        # Flattening the patches
-        batch = torch.permute(batch, [0, 2, 3, 4, 5, 1])
-        batch = torch.reshape(batch, [b, nh * nw, ph * pw * c])
-
-        batch = self.linear(batch)
-        cls: torch.Tensor = self.cls_token.expand([b, -1, -1])
-        emb: torch.Tensor = batch + self.pos_emb
-        return torch.cat([cls, emb], axis=1)
--- mmlearn/modules/layers/transformer_block.py
+++ mmlearn/modules/layers/transformer_block.py
@@ -1,8 +1,7 @@
 """Transformer Block and Embedding Modules for Vision Transformers (ViT)."""
 
-from typing import Callable, List, Optional, Union
+from typing import Callable, Optional, Union
 
-import numpy as np
 import torch
 from torch import nn
 
@@ -134,107 +133,3 @@ class Block(nn.Module):
             return attn
         x = x + self.drop_path(y)
         return x + self.drop_path(self.mlp(self.norm2(x)))
-
-
-class PatchEmbed(nn.Module):
-    """
-    Image to Patch Embedding.
-
-    This module divides an image into patches and embeds them as a sequence of vectors.
-
-    Parameters
-    ----------
-    img_size : int, optional, default=224
-        Size of the input image (assumed to be square).
-    patch_size : int, optional, default=16
-        Size of each image patch (assumed to be square).
-    in_chans : int, optional, default=3
-        Number of input channels in the image.
-    embed_dim : int, optional, default=768
-        Dimension of the output embeddings.
-
-    """
-
-    def __init__(
-        self,
-        img_size: int = 224,
-        patch_size: int = 16,
-        in_chans: int = 3,
-        embed_dim: int = 768,
-    ) -> None:
-        super().__init__()
-        num_patches = (img_size // patch_size) * (img_size // patch_size)
-        self.img_size = img_size
-        self.patch_size = patch_size
-        self.num_patches = num_patches
-
-        self.proj = nn.Conv2d(
-            in_chans, embed_dim, kernel_size=patch_size, stride=patch_size
-        )
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """Forward pass to convert an image into patch embeddings."""
-        return self.proj(x).flatten(2).transpose(1, 2)
-
-
-class ConvEmbed(nn.Module):
-    """
-    3x3 Convolution stems for ViT following ViTC models.
-
-    This module builds convolutional stems for Vision Transformers (ViT)
-    with intermediate batch normalization and ReLU activation.
-
-    Parameters
-    ----------
-    channels : List[int]
-        List of channel sizes for each convolution layer.
-    strides : List[int]
-        List of stride sizes for each convolution layer.
-    img_size : int, optional, default=224
-        Size of the input image (assumed to be square).
-    in_chans : int, optional, default=3
-        Number of input channels in the image.
-    batch_norm : bool, optional, default=True
-        Whether to include batch normalization after each convolution layer.
-
-    """
-
-    def __init__(
-        self,
-        channels: List[int],
-        strides: List[int],
-        img_size: int = 224,
-        in_chans: int = 3,
-        batch_norm: bool = True,
-    ) -> None:
-        super().__init__()
-        # Build the stems
-        stem = []
-        channels = [in_chans] + channels
-        for i in range(len(channels) - 2):
-            stem += [
-                nn.Conv2d(
-                    channels[i],
-                    channels[i + 1],
-                    kernel_size=3,
-                    stride=strides[i],
-                    padding=1,
-                    bias=(not batch_norm),
-                )
-            ]
-            if batch_norm:
-                stem += [nn.BatchNorm2d(channels[i + 1])]
-            stem += [nn.ReLU(inplace=True)]
-        stem += [
-            nn.Conv2d(channels[-2], channels[-1], kernel_size=1, stride=strides[-1])
-        ]
-        self.stem = nn.Sequential(*stem)
-
-        # Compute the number of patches
-        stride_prod = int(np.prod(strides))
-        self.num_patches = (img_size // stride_prod) ** 2
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """Forward pass through the convolutional embedding layers."""
-        p = self.stem(x)
-        return p.flatten(2).transpose(1, 2)
