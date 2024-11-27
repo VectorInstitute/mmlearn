5937565b96a098f683be602d8bc850e8c28ffa4c
fcogidi <41602287+fcogidi@users.noreply.github.com>
2024-11-27 15:30:47 -0500

    Fix import statement for IJEPA class

--- mmlearn/tasks/__init__.py
+++ mmlearn/tasks/__init__.py
@@ -1,7 +1,7 @@
 """Modules for pretraining, downstream and evaluation tasks."""
 
 from mmlearn.tasks.contrastive_pretraining import ContrastivePretraining
-from mmlearn.tasks.ijepa_pretraining import IJEPA
+from mmlearn.tasks.ijepa import IJEPA
 from mmlearn.tasks.zero_shot_classification import ZeroShotClassification
 from mmlearn.tasks.zero_shot_retrieval import ZeroShotCrossModalRetrieval
 
