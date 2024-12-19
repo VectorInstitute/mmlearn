12f9150ea6bf64a0bba1911d4d7585805201dd21
Negiiiin <neginbaghbanzadeh@gmail.com>
2024-12-19 11:32:23 -0500

    Fixed pre-commit issues

--- mmlearn/tasks/__init__.py
+++ mmlearn/tasks/__init__.py
@@ -2,9 +2,9 @@
 
 from mmlearn.tasks.contrastive_pretraining import ContrastivePretraining
 from mmlearn.tasks.ijepa import IJEPA
+from mmlearn.tasks.linear_probing import LinearClassifierModule
 from mmlearn.tasks.zero_shot_classification import ZeroShotClassification
 from mmlearn.tasks.zero_shot_retrieval import ZeroShotCrossModalRetrieval
-from mmlearn.tasks.linear_probing import LinearClassifierModule
 
 
 __all__ = [
